13615d26cad0519bcf5bec5fedaab46112d4ee8f
averageksp <158779203+averageksp@users.noreply.github.com>
2025-05-30 12:01:39 +0200

    Create AssemblyInfo.cs

---
+++ LanguageChanger/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LanguageChanger")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LanguageChanger")]
+[assembly: AssemblyCopyright("Copyright © averageksp 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("67fee094-f5d9-4778-890a-81b0f0389714")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
